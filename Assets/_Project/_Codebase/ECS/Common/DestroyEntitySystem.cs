4d5e5de352611d95f3b5dc6bad5ef8988cb1de1d
arewerage <arewerage@gmail.com>
2023-05-22 07:17:16 +0900

    Complete project

--- Assets/_Project/_Codebase/ECS/Common/DestroyEntitySystem.cs
+++ Assets/_Project/_Codebase/ECS/Common/DestroyEntitySystem.cs
@@ -1,4 +1,6 @@
-﻿using _Project._Codebase.ECS.UnityRelated;
+﻿using _Project._Codebase.ECS.Assets;
+using _Project._Codebase.ECS.Enemy;
+using _Project._Codebase.ECS.UnityRelated;
 using Unity.IL2CPP.CompilerServices;
 using UnityEngine;
 
@@ -11,15 +13,19 @@ namespace _Project._Codebase.ECS.Common
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     public sealed class DestroyEntitySystem : ISystem
     {
+        private Event<EnemyWaveSpawnRequest> _enemyWaveSpawnRequest;
         private Stash<Rigidbody2dComponent> _rigidbodyStash;
         private Filter _entities;
+        private Filter _enemies;
         
         public World World { get; set; }
 
         public void OnAwake()
         {
+            _enemyWaveSpawnRequest = World.GetEvent<EnemyWaveSpawnRequest>();
             _rigidbodyStash = World.GetStash<Rigidbody2dComponent>();
             _entities = World.Filter.With<DestroyComponent>().With<Rigidbody2dComponent>();
+            _enemies = World.Filter.With<EnemyMarker>().Without<AssetProviderComponent>();
         }
 
         public void OnUpdate(float deltaTime)
@@ -29,6 +35,9 @@ namespace _Project._Codebase.ECS.Common
                 ref var rigidbody = ref _rigidbodyStash.Get(entity);
                 
                 Object.Destroy(rigidbody.Value.gameObject);
+                
+                if (_enemies.IsEmpty())
+                    _enemyWaveSpawnRequest.NextFrame(new EnemyWaveSpawnRequest());
             }
         }
 
